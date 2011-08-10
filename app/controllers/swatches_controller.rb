f6e190edff8085f398ff508f7b0d15a0e4ec08f0
meleyal <william.meleyal@gmail.com>
2011-08-10 22:54:45 +0200

    cleanup

--- app/controllers/swatches_controller.rb
+++ app/controllers/swatches_controller.rb
@@ -2,14 +2,6 @@ class SwatchesController < ApplicationController
 
   # before_filter :authenticate
 
-  def pick(hash, *keys)
-    filtered = {}
-    hash.each do |key, value| 
-      filtered[key.to_sym] = value if keys.include?(key.to_sym) 
-    end
-    filtered
-  end
-
   def index
     @user = User.find_by_identifier(params[:user_id])
     if @user.nil?
@@ -24,18 +16,9 @@ class SwatchesController < ApplicationController
     render :json => Swatch.find(params[:id])
   end
   
-  def create
-    # Color.update_attributes pick(params, :title, :description)
-    # color = Color.find_or_create_by_value(pick(params, :value))
-    # debug params
-    
+  def create    
     # swatch = Swatch.create! params
-    
-    
     color = Color.find_or_create_by_value(params[:swatch][:colors][:value])
-    # logger.debug params
-    # logger.debug pick(params, :value)
-    # logger.debug "-----------------------#{params[:swatch][:colors][:value]}"
     @swatch = current_user.swatches.build(:color_id => color.id)
     @swatch.save
     render :json => @swatch
@@ -46,23 +29,5 @@ class SwatchesController < ApplicationController
     swatch.update_attributes! params
     render :json => swatch
   end
-
-  
-  # def create 
-  #   color = Color.find_or_create_by_value(params[:swatch][:colors][:value])    
-  #   @swatch = current_user.swatches.build(:color_id => color.id)
-  # 
-  #   if @swatch.save
-  #     flash[:success] = "Saved"
-  #   else
-  #     flash[:error] = color.errors.empty? ? "Error" : color.errors.full_messages.to_sentence                    
-  #   end
-  #   
-  #   respond_to do |format|
-  #     format.html { redirect_to user_path(current_user) }
-  #     format.js   { render :partial => 'swatches/swatch', :locals => { :link => false } }
-  #   end
-  #   
-  # end
 
 end
